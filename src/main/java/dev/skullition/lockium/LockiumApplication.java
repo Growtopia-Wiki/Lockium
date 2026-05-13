f207b2bdc4653707480d8f0e90ed65428e001508
Skullie <78258797+sskullie@users.noreply.github.com>
2026-05-13 11:13:19 +0700

    Initial commit

---
+++ src/main/java/dev/skullition/lockium/LockiumApplication.java
@@ -0,0 +1,13 @@
+package dev.skullition.lockium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockiumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LockiumApplication.class, args);
+	}
+
+}
