9467e1d29fd6f5b8735754b33f1022ecfe09abd1
Skullie <78258797+sskullie@users.noreply.github.com>
2026-05-20 17:58:30 +0700

    Improve header for item containers

--- src/main/java/dev/skullition/lockium/util/ItemUtils.java
+++ src/main/java/dev/skullition/lockium/util/ItemUtils.java
@@ -3,8 +3,10 @@ package dev.skullition.lockium.util;
 import dev.skullition.lockium.model.ItemDetailResponse;
 import net.dv8tion.jda.api.components.container.Container;
 import net.dv8tion.jda.api.components.container.ContainerChildComponent;
+import net.dv8tion.jda.api.components.section.Section;
 import net.dv8tion.jda.api.components.separator.Separator;
 import net.dv8tion.jda.api.components.textdisplay.TextDisplay;
+import net.dv8tion.jda.api.components.thumbnail.Thumbnail;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +15,7 @@ import java.util.Locale;
 public class ItemUtils {
     private static final String ITEM_SPRITE_URL = "https://cdn.growtopiawiki.com/sprites/%s.png";
     private static final String TREE_SPRITE_URL = "https://cdn.growtopiawiki.com/sprites/%s-tree.png";
+    private static final String GROWTOPIA_WIKI_URL = "https://growtopiawiki.com/w/%s";
 
     public static String getItemSpriteUrl(int id) {
         return String.format(ITEM_SPRITE_URL, id);
@@ -30,7 +33,13 @@ public class ItemUtils {
         List<ContainerChildComponent> container = new ArrayList<>();
 
         // 1. Add Header
-        container.add(TextDisplay.of(String.format("# %s", item.item().name())));
+        String itemUrl = String.format(GROWTOPIA_WIKI_URL, getWikiItemName(item.item().name()));
+        Section header = Section.of(Thumbnail.fromUrl(
+                        getItemSpriteUrl(item.item().id())),
+                TextDisplay.of(String.format("## [%s](%s)", item.item().name(), itemUrl)),
+                TextDisplay.of(item.item().description())
+        );
+        container.add(header);
         container.add(Separator.create(true, Separator.Spacing.LARGE));
 
         // 2. Add Middle Components
@@ -41,5 +50,9 @@ public class ItemUtils {
         container.add(TextDisplay.of("-# With love, by the [Growtopia Wiki](https://growtopiawiki.com)."));
 
         return Container.of(container).withAccentColor(item.seed().overColor().intOrTransparent());
+    }
+
+    public static String getWikiItemName(String itemName) {
+        return itemName.replace(" ", "_");
     }
 }
