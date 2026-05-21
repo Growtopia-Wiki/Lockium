3bd16901ec8128e8a3184aeefdeb56a8b909d51e
Skullie <78258797+sskullie@users.noreply.github.com>
2026-05-21 15:01:25 +0700

    Use application emojis

---
+++ src/main/java/dev/skullition/lockium/util/AppEmojis.java
@@ -0,0 +1,30 @@
+package dev.skullition.lockium.util;
+
+import io.github.freya022.botcommands.api.emojis.annotations.AppEmojiContainer;
+import net.dv8tion.jda.api.entities.emoji.ApplicationEmoji;
+
+import static io.github.freya022.botcommands.api.emojis.AppEmojisRegistry.get;
+
+@AppEmojiContainer
+public class AppEmojis {
+    public static final ApplicationEmoji RARITY = get("RARITY");
+    public static final ApplicationEmoji CHECKBOX_ENABLED = get("CHECKBOX_ENABLED");
+    public static final ApplicationEmoji CHECKBOX_DISABLED = get("CHECKBOX_DISABLED");
+    public static final ApplicationEmoji FAIRY_WINGS = get("FAIRY_WINGS");
+    public static final ApplicationEmoji TICKING_CLOCK = get("TICKING_CLOCK");
+    public static final ApplicationEmoji BATTLE_LEASH = get("BATTLE_LEASH");
+    public static final ApplicationEmoji EARTH = get("EARTH");
+    public static final ApplicationEmoji FIRE = get("FIRE");
+    public static final ApplicationEmoji MISSING = get("MISSING");
+    public static final ApplicationEmoji WATER = get("WATER");
+    public static final ApplicationEmoji WIND = get("WIND");
+    public static final ApplicationEmoji GEMS = get("GEMS");
+    public static final ApplicationEmoji GROWTOKEN = get("GROWTOKEN");
+    public static final ApplicationEmoji LOCKE = get("LOCKE");
+    public static final ApplicationEmoji SPRING_CHEST = get("SPRING_CHEST");
+    public static final ApplicationEmoji SUMMER_CHEST = get("SUMMER_CHEST");
+    public static final ApplicationEmoji WINTER_CHEST = get("WINTER_CHEST");
+    public static final ApplicationEmoji CHALLENGE_BOARD = get("CHALLENGE_BOARD");
+    public static final ApplicationEmoji LEGENDARY_WIZARD = get("LEGENDARY_WIZARD");
+    public static final ApplicationEmoji FISHING_ROD = get("FISHING_ROD");
+}
